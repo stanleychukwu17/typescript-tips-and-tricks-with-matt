685d562622ea1455ab4a60fd1e02ab0899f95043
stanleychukwu17 <stanleychukwu17@yahoo.com>
2022-07-30 08:18:40 +0100

    finished the note on example 2

--- src/components/m2/App.ts
+++ src/components/m2/App.ts
@@ -21,32 +21,32 @@ console.log(value)
 
 
 /**
-solution 1: So generics to the rescue this problem
+solution 1: So generics to the rescue this problem, un-comment the return lines to see errors
 */
 export const getDeepValue_1 = <TObj>(obj:TObj, firstKey:string, secondKey:string) => {
-    return obj[firstKey][secondKey]
+    // return obj[firstKey][secondKey]
 }
 
 /**
 So now, typescript is complaining that it cannot access an unknown object since it doesn't know what type of value of whatever we have passed in,
 first it doesn't know we even passed in an object... so take a look at the near final solution
-solution 2: So
+solution 2: un-comment the return lines to see errors
 */
 export const getDeepValue_2 = <TObj>(
     obj:TObj,
     firstKey:keyof TObj,
     secondKey:string
 ) => {
-    return obj[firstKey][secondKey]
+    // return obj[firstKey][secondKey]
 }
 
-// next:
+// next: un-comment the return lines to see errors
 export const getDeepValue_3 = <TObj, TFirstKey extends keyof TObj>(
     obj:TObj,
     firstKey:TFirstKey,
     secondKey:string
 ) => {
-    return obj[firstKey][secondKey]
+    // return obj[firstKey][secondKey]
 }
 
 // finally:
@@ -59,5 +59,9 @@ export const getDeepValue_4 = <TObj, TFirstKey extends keyof TObj, TSecondKey ex
 }
 
 /**
+--keyof-- can be used to extract the keys of any object
 so hopefully you understand what we've done here.. otherWise head over to the youtube video and watch the full video to see the teacher explain
+so this all about using some extensions provided by typescript to solve some common problems.. in this case, how do we access a generic object
+when we are not sure of what the keys and values of this object would be..
+you can also do --TObj extends object-- just to be sure that TObj is always an object
 */
