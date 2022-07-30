01a2f826ec0fbcc8fa83bb906f65b00a011b252f
stanleychukwu17 <stanleychukwu17@yahoo.com>
2022-07-30 07:56:26 +0100

    cleaned up m2 which is example2 finished working on example 2 showing how to use keyof to grab the key of an unknown generic object the is going to be passed in

--- src/components/m2/App.ts
+++ src/components/m2/App.ts
@@ -1,5 +1,7 @@
 /**
-So the problem here is that 
+So the problem here is that we have this function below, we want the function to be able to access  deeper into any objects passed to
+it as an argument, but the thing is that we want the object key and values to be dynamic, we do not want fixed object keys and values.
+so here is the problem below:
 */
 export const getDeepValue = (obj:any, firstKey:string, secondKey:string) => {
     return obj[firstKey][secondKey]
@@ -15,18 +17,47 @@ const obj = {
 }
 
 const value = getDeepValue(obj, "foo", "b")
+console.log(value)
 
 
 /**
-So now, what if we wanted to pass in any type of object with having values of any types, we can use generics to solve this problem
-solution 1
+solution 1: So generics to the rescue this problem
 */
 export const getDeepValue_1 = <TObj>(obj:TObj, firstKey:string, secondKey:string) => {
     return obj[firstKey][secondKey]
 }
 
 /**
-So now, typescript is complaining that it cannot access an unknown object since it doesn't know what type of value of what ever we have passed in,
+So now, typescript is complaining that it cannot access an unknown object since it doesn't know what type of value of whatever we have passed in,
 first it doesn't know we even passed in an object... so take a look at the near final solution
-solution 2
+solution 2: So
+*/
+export const getDeepValue_2 = <TObj>(
+    obj:TObj,
+    firstKey:keyof TObj,
+    secondKey:string
+) => {
+    return obj[firstKey][secondKey]
+}
+
+// next:
+export const getDeepValue_3 = <TObj, TFirstKey extends keyof TObj>(
+    obj:TObj,
+    firstKey:TFirstKey,
+    secondKey:string
+) => {
+    return obj[firstKey][secondKey]
+}
+
+// finally:
+export const getDeepValue_4 = <TObj, TFirstKey extends keyof TObj, TSecondKey extends keyof TObj[TFirstKey]>(
+    obj:TObj,
+    firstKey:TFirstKey,
+    secondKey:TSecondKey
+) => {
+    return obj[firstKey][secondKey]
+}
+
+/**
+so hopefully you understand what we've done here.. otherWise head over to the youtube video and watch the full video to see the teacher explain
 */
