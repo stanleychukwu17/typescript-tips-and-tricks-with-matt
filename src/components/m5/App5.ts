0aff40fd503abc75dfa457f453942d90956c3fa0
stanleychukwu17 <stanleychukwu17@yahoo.com>
2022-08-01 11:33:38 +0100

    done with refactoring, more examples coming in!

--- src/components/m5/App5.ts
+++ src/components/m5/App5.ts
@@ -1,6 +1,6 @@
 /**
-so now the function below is meant to compare two values and tell you if those two values are the same, but we all know that
-with arrays, even if they are the same thing, it will never return true, so we do not want the objects to compare arrays
+so now the function below is meant to compare two values and tells you if those two values are the same, but we all know that
+with arrays, even if they are the same thing, it will never return true, so we do not want the function to compare arrays
 */
 export const deepEqualCompare = <Arg>(
     a: Arg,
@@ -17,7 +17,7 @@ solution 1
 we can use if else in typescript to make sure that the arguments are not arrays
 */
 export const deepEqualCompare_1 = <Arg>(
-    a: Arg extends any[] ? `Don't pass an array!` : Arg,
+    a: Arg extends any[] ? `Don't pass an array!` : Arg, // i would love to do: 'a: Arg extends any[] ? never : Arg'
     b: Arg
 ): boolean => {
     if (Array.isArray(a) || Array.isArray(b)) {
@@ -29,6 +29,7 @@ export const deepEqualCompare_1 = <Arg>(
 // see different use cases, un-comment to see errors
 const c1 = deepEqualCompare_1('a', 'b')
 const c2 = deepEqualCompare_1<string>('a', 'b')
-// const c3 = deepEqualCompare_1<number>('a', 'b')
+console.log(c1, c2)
+// const c3 = deepEqualCompare_1<number>('a', 'b') // this will give you an error, because you've set the generic to a number but you're passing in string as an argument
 // const c4 = deepEqualCompare_1<[]>('a', 'b')
 // const c5 = deepEqualCompare_1([], [])
