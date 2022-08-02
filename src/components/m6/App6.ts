2105b142d2ed137645c5b993296d156cdb6ef657
stanleychukwu17 <stanleychukwu17@yahoo.com>
2022-08-02 18:07:14 +0100

    done with example 6 that just shows a pure example of advanced typescript

--- src/components/m6/App6.ts
+++ src/components/m6/App6.ts
@@ -4,9 +4,9 @@ export const query = `/home?a=foo&b=bow`
 
 type Query = typeof query
 
-type SecondQueryPart = String.Split<Query, "?">[1]
+type SecondQueryPart = String.Split<Query, "?">[1] // ["/home", "a=foo&b=bow"], so doing [1] gives us "a=foo&b=bow"
 
-type QueryElements = String.Split<SecondQueryPart, "&">
+type QueryElements = String.Split<SecondQueryPart, "&"> // split-"a=foo&b=bow" now gives us ["a=foo", "b=bow"]
 
 type QueryParams = {
     [QueryElement in QueryElements[number]]: {
@@ -15,8 +15,18 @@ type QueryParams = {
     }
 }[QueryElements[number]]
 
+
+/**
+So am going to explain the code above for you below
+QueryElements[number] this is like a function in typescript, because we passed in number into the [], typescript will automatically loop through every value in it,
+even if you do the same for objects, it will loop through the object key and values, but return only the values as a union, but use 'name_of_type[numbers]' mainly when your
+type is an array
+
+I stopped explaining this code since it is taking up a-lot of my time and i feel that this is an example of a simple problem that has been over engineered
+*/
+
 const obj: Union.Merge<QueryParams> = {
     a: "foo",
-    b: "wow"
+    b: "bow"
 }
 console.log(obj)
