777e3eaecf72bdb777e9a3a190aeab87a9011aae
stanleychukwu17 <stanleychukwu17@yahoo.com>
2022-08-08 19:39:20 +0100

    updated the commenting in example 1

--- src/components/m1/App1.tsx
+++ src/components/m1/App1.tsx
@@ -1,7 +1,7 @@
 /**
 this example is all about using of generics
-in the TableProps, items is an object that is limited to an array that contains only {id:string; firstName: string}, but we can use generics to make items an array of 
-multi-dimensional objects, where items can be any type of objects within it's array ... see solution
+in the TableProps, 'items' is an object that is limited to an array that contains only '{id:string; firstName: string}', but we can use generics to make 'items' an array of 
+multi-dimensional objects, where 'items' can be any type of objects within it's array ... see solution
 */
 interface TableProps {
     items: {id:string; firstName: string}[];
@@ -33,12 +33,12 @@ interface TableProps2<anyItem> {
     renderItems: (item: anyItem) => React.ReactNode
 }
 
-// without the ,(comma) after the anyItem, typeScript would trow an error, also you can call anyItem any names you want e.g you can change the name to box and there would be no errors
+// without the ,(comma) after the anyItem, typeScript would throw an error, also you can call anyItem any names you want e.g you can change the name to box and there would be no errors
 export const Table2 = <anyItem,>(props: TableProps2<anyItem>) => {
     return <>Hi</>
 }
 
-// so now, if you hover over the each of dts argument passed into the renderItems functions, you'll see that dts has all the values of whatever the items object received
+// so now, if you hover over the each of 'dts' argument passed into the renderItems functions, you'll see that 'dts' has all the values of whatever the items object received
 // so now, we can have a table for our users, our pets, our foodstuff and e.t.c
 export const Component2 = () => {
     return (
